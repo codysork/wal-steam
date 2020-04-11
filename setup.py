e89eddd5abc89916abbad929aeef278f3cf141a8
Cody Sork <codysork@tutanota.com>
2020-04-10 21:26:46 -0700

    Changed file name and edited setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-"""wal_steam - setup.py"""
+"""wal-steam - setup.py"""
 import setuptools
 
 try:
     import wal_steam
 except (ImportError):
-    print("error: wal_steam requires Python 3.5 or greater.")
+    print("error: wal-steam requires Python 3.5 or greater.")
     quit(1)
 
 VERSION = wal_steam.VERSION
 DOWNLOAD = "https://github.com/codysork/wal_steam/archive/%s.tar.gz" % VERSION
 
 setuptools.setup(
-    name="wal_steam",
+    name="wal-steam-codys-fork",
     version=VERSION,
     author="Cody Sork",
     author_email="codysork@pm.me",
-    description="A fork of Dakota Walsh's original wal_steam script.",
+    description="""A fork of Dakota Walsh's original wal_steam script.
+    Copyright (C) 2019 Dakota Walsh""",
     license="MIT",
-    url="https://github.com/codysork/wal_steam",
+    url="https://github.com/codysork/wal-steam",
     download_url=DOWNLOAD,
     classifiers=[
         "Environment :: X11 Applications",
@@ -27,9 +28,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
     ],
     install_requires="pywal >= 0.6.7",
-    scripts=['wal_steam.py'],
+    scripts=['wal-steam.py'],
     entry_points={
-        "console_scripts": ["wal-steam=wal_steam:main"]
+        "console_scripts": ["wal-steam=wal-steam:main"]
     },
     python_requires=">=3.5",
     include_package_data=True
